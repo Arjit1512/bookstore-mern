1f5aaedc8c8cb3536d82c531e6061670ddb8da96
Arjit1512 <125079524+Arjit1512@users.noreply.github.com>
2023-09-22 19:02:51 +0530

    Update CreateBooks.jsx

--- frontend/pages/CreateBooks.jsx
+++ frontend/pages/CreateBooks.jsx
@@ -22,7 +22,7 @@ const CreateBooks = () => {
     setLoading(true);
 
     axios
-    .post('http://localhost:5555/books',data)
+    .post('https://bookstore-mern-4qn8.onrender.com/books',data)
     
     .then( () => {
       setLoading(false);
