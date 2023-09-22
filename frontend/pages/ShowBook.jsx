f72d8de7a4cbca7fa16eb10f20f6b1b625b3212a
Arjit1512 <125079524+Arjit1512@users.noreply.github.com>
2023-09-22 19:41:21 +0530

    Update ShowBook.jsx

--- frontend/pages/ShowBook.jsx
+++ frontend/pages/ShowBook.jsx
@@ -15,7 +15,7 @@ const ShowBook = () => {
   useEffect( () => {
     setLoading(true);
     axios
-    .get(`https://bookstore-mern-4qn8.onrender.com/books/${id}`)
+    .get(`http://localhost:5555/books/${id}`)
     .then( (response) => {
        setBook(response.data);
        setLoading(false);
