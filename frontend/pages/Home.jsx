7b643165cc686cc1cf5035fb4b4ddfb34408d357
Arjit1512 <125079524+Arjit1512@users.noreply.github.com>
2023-09-22 18:59:58 +0530

    Update Home.jsx

--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
     useEffect( () => {
         setLoading(true);
         axios
-        .get('http://localhost:5555/books')
+        .get('https://bookstore-mern-4qn8.onrender.com/books')
         .then( (response) => {
             setBooks(response.data.data);
             setLoading(false);
