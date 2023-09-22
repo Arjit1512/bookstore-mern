95f2d115439a0031a3534398cd0406c3b10fc308
Arjit1512 <125079524+Arjit1512@users.noreply.github.com>
2023-09-22 19:54:00 +0530

    Update DeleteBook.jsx

--- frontend/pages/DeleteBook.jsx
+++ frontend/pages/DeleteBook.jsx
@@ -15,7 +15,7 @@ const DeleteBook = () => {
     setLoading(true);
   
   axios
-  .delete(`http://localhost:5555/books/${id}`)
+  .delete(`https://bookstore-mern-4qn8.onrender.com/books/${id}`)
 
   .then( () => {
     setLoading(false);
